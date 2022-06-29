067dacdd983eb644d3d0591c3ef957be08d38db4
Tobias <tobiasnordahl@gmail.com>
2022-06-29 17:48:47 +0200

    update frontpage

--- storytime.js
+++ storytime.js
@@ -11,17 +11,20 @@ let webpage = new Object();
 
 // Frontpage
 webpage.frontpage = {
-  name: "Tobias Nordahl",
-  address: "8260 Viby J · Midtjylland, Danmark · (+45) 40 30 44 61",
-  email: "post@tobiasnordahl.dk",
-  summary: `Analytiker - dog også kodekværner, datagnaver, front-end designer 🎉 👩‍💻, med passion for mange tekniske (HTML,CSS,JS,PHP,SQL,LINUX++🚀) og kommunikative aspekter (ADFÆRDSDESIGN,PROCESOPTIMERING & HÅNDTERING).`,
-  links: [
-    {
-      name: "LinkedIn",
-      href: "https://www.linkedin.com/in/tobias-nordahl-43b295103/",
-    },
-    { name: "GitHub", href: "https://github.com/exetico/" },
-  ],
+  title: "<HEAD>",
+  body: {
+    name: "Tobias Nordahl",
+    address: "8260 Viby J · Midtjylland, Danmark · (+45) 40 30 44 61",
+    email: "post@tobiasnordahl.dk",
+    summary: `Analytiker - dog også kodekværner, datagnaver, front-end designer 🎉 👩‍💻, med passion for mange tekniske (HTML,CSS,JS,PHP,SQL,LINUX++🚀) og kommunikative aspekter (ADFÆRDSDESIGN,PROCESOPTIMERING & HÅNDTERING).`,
+    links: [
+      {
+        name: "LinkedIn",
+        href: "https://www.linkedin.com/in/tobias-nordahl-43b295103/",
+      },
+      { name: "GitHub", href: "https://github.com/exetico/" },
+    ],
+  },
 };
 
 /*
